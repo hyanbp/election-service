41704851d3671bad1eb72f8f9846009b991bda4b
Hyan Porciuncula <ntconsult.hyan@agiplan.com>
2019-02-28 14:12:15 -0300

    first commit

---
+++ src/main/java/com/hyan/electionservice/ElectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hyan.electionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyan/electionservice/ElectionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyan.electionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
