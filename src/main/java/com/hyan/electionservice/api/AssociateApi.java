418651780137dcda38b511125df2fb5a8531ab68
hyan <hyanbp@gmail.com>
2019-03-02 19:57:20 -0300

    adicionando logs nas api

--- src/main/java/com/hyan/electionservice/api/AssociateApi.java
+++ src/main/java/com/hyan/electionservice/api/AssociateApi.java
@@ -7,6 +7,8 @@ import com.hyan.electionservice.api.response.ElectionResponse;
 import com.hyan.electionservice.entity.Associate;
 import com.hyan.electionservice.service.AssociateService;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +20,23 @@ import reactor.core.publisher.Mono;
 @Validated
 public class AssociateApi {
 
-
+    private Logger logger = LoggerFactory.getLogger(ElectionApi.class);
     private AssociateService associateService;
 
     public AssociateApi(AssociateService associateService) {
         this.associateService = associateService;
+
     }
+
 
     @PostMapping
     @ApiOperation(value = "Cria um associado.")
     public Mono<AssociateResponse> postElection(AssociateRequest request) {
 
+        logger.info("Criando um Associado. CPF [{}]", request.getTaxId());
         return associateService.create(request.getTaxId())
-                .map(x -> new AssociateResponse(x.getTaxId(), x.isAlreadyVoted()));
+                .map(x -> new AssociateResponse(x.getTaxId(), x.isAlreadyVoted()))
+                .doOnSuccess(y -> logger.info("Criação de Associado realizado com sucesso. CPF [{}]", y.getTaxId()));
     }
 
 
--- src/main/java/com/hyan/electionservice/api/ElectionApi.java
+++ src/main/java/com/hyan/electionservice/api/ElectionApi.java
@@ -8,6 +8,8 @@ import com.hyan.electionservice.entity.DecisionType;
 import com.hyan.electionservice.service.ElectionService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -24,14 +26,16 @@ public class ElectionApi {
         this.electionService = electionService;
         this.objectMapper = objectMapper;
     }
+    private Logger logger = LoggerFactory.getLogger(ElectionApi.class);
 
 
     @PostMapping
-    @ApiOperation(value = "Cria uma pauta de votação")
+    @ApiOperation(value = "Cria uma eleição/pauta de votação")
     public Mono<ElectionResponse> postElection(ElectionRequest request) {
-
+        logger.info("Criando uma eleição/pauta de votação, Nome:[{}]", request.getName());
         return electionService.create(request.getName(), request.getExpirationToMinutes())
-                .map(x -> new ElectionResponse(x));
+                .map(x -> new ElectionResponse(x))
+                .doOnSuccess(y -> logger.info("Eleição criada com sucesso. Codigo: [{}]", y.getElectionCode()));
     }
 
     @PostMapping("/{electionCode}/vote")
@@ -39,16 +43,19 @@ public class ElectionApi {
     public Mono<Void> postVote(@PathVariable String electionCode,
                                 @RequestParam @ApiParam("Escolha do voto (SIM, NAO)") DecisionType decisionType,
                                 @RequestParam String  associate) {
-
-        return electionService.vote(electionCode,decisionType.name(),associate);
+        logger.info("Iniciando votação para a eleição/pauta:[{}] para o Associado:[{}]", electionCode, associate);
+        return electionService.vote(electionCode,decisionType.name(),associate)
+                .doOnSuccess(y ->  logger.info("Votação realizada com sucesso para o Associado: [{}]", associate));
     }
 
 
     @GetMapping("/{electionCode}/result")
     @ApiOperation(value = "Resultado da votação")
     public Mono<ResultVoteResponse> getResultVote(@PathVariable String electionCode) {
+        logger.info("Inicando busca pelo resultado da Eleição. Código da eleição [{}]",electionCode);
         return electionService.resultVote(electionCode)
-                .map(el  ->  new ResultVoteResponse(el.getYes(),el.getNo()));
+                .map(el  ->  new ResultVoteResponse(el.getYes(),el.getNo()))
+                .doOnSuccess(x -> logger.info("Busca de resultado realizado com sucesso para o Código da eleição [{}]", electionCode));
     }
 
 
